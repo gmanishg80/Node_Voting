26b598597175bfd75162932705baa89c2a957b77
ManishKumar970 <kmanishk970@gmail.com>
2024-06-10 14:40:54 +0530

    update in jwt

--- controllers/voter.controller.js
+++ controllers/voter.controller.js
@@ -13,34 +13,31 @@ const signup = [
         return res.status(400).json({ errors: errors.array() });
       }
 
-      const voterData = req.body;  // Assuming the request body contains the Voter data
+      const voterData = req.body; // Assuming the request body contains the Voter data
 
-      
       // Check if a Voter with the same Aadhar Card Number already exists
       const existingVoter = await Voter.findOne({
         aadharCardNumber: voterData.aadharCardNumber,
       });
       if (existingVoter) {
-        return res
-          .status(400)
-          .json({
-            error: "Voter with the same Aadhar Card Number already exists",
-          });
+        return res.status(400).json({
+          error: "Voter with the same Aadhar Card Number already exists",
+        });
       }
 
-      const newVoter = new Voter(voterData);   // Create a new User document using the Mongoose model
-      const response = await newVoter.save();   // Save the new user to the database
+      const newVoter = new Voter(voterData); // Create a new User document using the Mongoose model
+      const response = await newVoter.save(); // Save the new user to the database
       console.log("data saved");
 
       const payload = {
         id: response.id,
       };
-      console.log(JSON.stringify(payload));  //printing payload
+      console.log(JSON.stringify(payload)); //printing payload
       const token = generateToken(payload);
 
       res.status(200).json({ response: response, token: token });
     } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error'});
+      res.status(500).json({ error: "Internal Server Error" });
     }
   },
 ];
